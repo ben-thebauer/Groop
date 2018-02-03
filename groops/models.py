0511cbf9f8319dc60e1ae2a57b234273b83a240d
Unknown <benbauer314@gmail.com>
2018-02-03 08:51:38 -0800

    Basic working groops django app

--- groops/models.py
+++ groops/models.py
@@ -4,20 +4,12 @@ from django.db import models
 class Groop(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
+    logo = models.ImageField()
+    website = models.URLField()
+    #foreignkey schedule
+    #foeignkey members
+
     #CAN ADD ANY CLASS METHODS HERE
     #I THINK YOU CAN TREAT FIELDS LIKE NORMAL SELF.VARIABLES
     def __str__(self):
         return self.name
-
-class Event(models.Model):
-    groop = models.ForeignKey(Groop, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    location = models.CharField(max_length=100)
-    date = models.DateTimeField()
-
-    def __str__(self):
-        return self.name
-
-    def isDopeEvent(self):
-        return self.groop.name ==  'Alpha Epsilon Pi'
--- groops/views.py
+++ groops/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Groop
 # Create your views here.
 def index(request):
-    return HttpResponse("This is where the groops will go")
+    template = loader.get_template('groops/index.html')
+    context = {'groops' : Groop.objects}#can add objects to this dictionary and then pass them into method below to display on page
+    return HttpResponse(template.render(context, request))
